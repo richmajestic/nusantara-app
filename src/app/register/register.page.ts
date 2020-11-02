070d939ed16ad1b76939174c4a0b73fa3345c786
yunika5699 <yunikalaurensiaw@gmail.com>
2020-11-02 15:54:30 +0700

    Update font and scss, Register page

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MaungImage } from "../../assets";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  @Input() namaText : string;
+  @Input() usernameText: string;
+  @Input() passwordText: string;
+  errorText: string = "";
+
+  public maungSource = MaungImage;
+
+  constructor(private route : Router) { }
+
+  ngOnInit() {
+  }
+
+  onClickRegister() {
+    if (!this.usernameText || !this.passwordText || !this.namaText) {
+      this.errorText = "Mohon isi semua field";
+      return;
+    }
+  }
+
+  toLogin(){
+    this.route.navigateByUrl('/login');
+  }
+
+}
