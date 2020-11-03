03f0d95c3af3e72b2ce5c4a5696e54b9dd071aaf
Andre Irawan <andreirawan97@gmail.com>
2020-11-04 00:06:26 +0700

    Add half-baked home tab content

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { MaungImage } from "src/assets";
 
 @Component({
   selector: "app-tab1",
@@ -6,7 +7,27 @@ import { Component } from "@angular/core";
   styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-  public contents;
+  public maungSource = MaungImage;
+  public currentCategory: string = "BANGUNAN IKONIK";
+  public currentProgress: number = 2; // Progress user
+  public totalLevel: number = 10; // Total level dari kategori terakhir
+  public arrayOfKategori = [
+    {
+      nama: "FLORA & FAUNA",
+      jumlahPertanyaan: 25,
+      warna: "#D1F4E6",
+    },
+    {
+      nama: "BANGUNAN IKONIK",
+      jumlahPertanyaan: 10,
+      warna: "#FBDCDB",
+    },
+    // {
+    //   nama: "RUMAH MAKAN",
+    //   jumlahPertanyaan: 10,
+    //   warna: "#D1F4E6",
+    // },
+  ];
 
   constructor() {}
 }
